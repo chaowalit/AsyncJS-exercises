c41cbaa36057dfd09f2dac38c7b982c0bc37e7e9
chaowalit <dekchaowalit_123@hotmail.com>
2017-11-21 10:31:20 +0700

    first commit

--- async.js
+++ async.js
@@ -1,7 +1,11 @@
 const request = require('request');
 
 const test = () => {
-  
+    request('https://google.com', (error, response, body) => {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        //console.log('body:', body); // Print the HTML for the Google homepage.
+    }); 
 }
 //START OF PROGRAM
 console.log("Hitting google");
